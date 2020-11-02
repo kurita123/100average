a54c4c9f89f264af054aaf21a159ea46c04fc8ca
kurita <kurikuri7890@icloud.com>
2020-11-03 01:18:41 +0900

    マイグレーションファイル追加、モデル追加、トップページヘッダー変更

---
+++ backend/app/Http/Controllers/TopController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TopController extends Controller
+{
+    public function top(Request $request){
+        
+        
+
+        return view('top');
+    }
+}
---
+++ backend/resources/views/top.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title','お得な商品発見')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        
+    </div>
+</div>
+@endsection
